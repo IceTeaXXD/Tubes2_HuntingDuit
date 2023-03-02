9760bfbc22f35ee2e5c4907a12aa67a93ca215f4
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-03-02 16:48:34 +0700

    feat: added initial file and txt to matrix read

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // Read the text file
+        string[] lines = File.ReadAllLines("test/maze1.txt");
+
+        // Create a 2D matrix to store the data
+        char[,] matrix = new char[lines.Length, lines[0].Split(' ').Length];
+
+        // Fill the matrix with data from the text file
+        for (int i = 0; i < lines.Length; i++)
+        {
+            string[] values = lines[i].Split(' ');
+            for (int j = 0; j < values.Length; j++)
+            {
+                matrix[i, j] = values[j][0];
+            }
+        }
+
+        // Print the matrix to the console
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
