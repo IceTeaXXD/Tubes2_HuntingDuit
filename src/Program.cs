3662e9119b7e0940a15a717ba9ed6413a72b7612
IceTeaXXD <13521024@std.stei.itb.ac.id>
2023-03-12 18:30:06 +0700

    feat: added filename input

--- src/Program.cs
+++ src/Program.cs
@@ -3,14 +3,23 @@ using System.IO;
 using System.Collections.Generic;
 using NodeSpace;
 using GraphSpace;
-
-class Program {
-    static void Main(string[] args) {
-        Graph graph = new Graph();
-        string filename = "../test/maze1.txt";
-        graph.makeGraph(filename);
-
-        // Print adjacency list
-        graph.PrintGraph();
+class Program
+{
+    static void Main(string[] args)
+    {
+        Graph map = new Graph();
+        string filename;
+        while (true){
+            Console.Write("Enter filename: ");
+            filename = Console.ReadLine();
+            try {
+                string[] lines = System.IO.File.ReadAllLines("../test/" + filename);
+                map.makeGraph("../test/" + filename);
+                break;
+            } catch (System.IO.FileNotFoundException) {
+                Console.WriteLine("File not found");
+            }
+        }
+        map.PrintGraph();
     }
 }
